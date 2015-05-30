70c4da239561769c23aa3cbcaf3b9f4edc97ab7b
Sam Gleske <sam.mxracer@gmail.com>
2015-05-30 19:02:12 -0400

    Update the slackToken to be trogdor slack
    integration user

--- scripts/configure-slack.groovy
+++ scripts/configure-slack.groovy
@@ -3,11 +3,11 @@ def slack = Jenkins.instance.getExtensionList(
 )[0]
 //valid tokens for testing in the jenkins-slack-plugin-test instance of slack.com
 def params = [
-  slackTeamDomain: "jenkins-slack-plugin",
-  slackToken: "xaMSYVZ0GgGbAzIOhqN2fviO",
-  slackRoom: "#slack-plugin-testing #random",
-  slackBuildServerUrl: "http://localhost:8080/",
-  slackSendAs: ""
+  slackTeamDomain: 'jenkins-slack-plugin',
+  slackToken: 'jfG4cs9hT6QsRON2ocwXx980',
+  slackRoom: '#slack-plugin-testing #random',
+  slackBuildServerUrl: 'http://localhost:8080/',
+  slackSendAs: ''
 ]
 def req = [
   getParameter: { name -> params[name] }
@@ -15,4 +15,4 @@ def req = [
 slack.configure(req, null)
 
 slack.save()
-println "Slack global settings configured."
+println 'Slack global settings configured.'
