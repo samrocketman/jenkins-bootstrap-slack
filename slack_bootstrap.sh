74e6d2b570727dabdbb227d784fcad8208d8a3c6
Sam Gleske <sam.mxracer@gmail.com>
2015-05-30 19:02:59 -0400

    Configure a more advanced unit testing job
    I decided to use jervis because it is a groovy library and self
    bootstraps.  This will work on most systems.

--- slack_bootstrap.sh
+++ slack_bootstrap.sh
@@ -13,11 +13,14 @@ export JENKINS_HOME="my_jenkins_home"
 #download jenkins, start it up, and update the plugins
 ./scripts/provision_jenkins.sh bootstrap | grep -v 'Jenkins is ready'
 #install required plugins
-./scripts/provision_jenkins.sh install-plugins groovy slack
+#cobertura is for code coverage reporting
+#covcomplplot is for surfacing code complexity graphs
+./scripts/provision_jenkins.sh install-plugins slack cobertura covcomplplot
 #restart jenkins
 ./scripts/provision_jenkins.sh restart
 #create the jobs that are well configured for testing the slack plugin.
 ./scripts/provision_jenkins.sh cli create-job Test < ./configs/job_Test_config.xml
+./scripts/provision_jenkins.sh cli create-job jervis < ./configs/job_jervis_config.xml
 #configure the slack global settings
 curl -d "script=$(<./scripts/configure-slack.groovy)" http://localhost:8080/scriptText
 echo 'Jenkins is ready.  Visit http://localhost:8080/'
