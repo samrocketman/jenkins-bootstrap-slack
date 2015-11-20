9f2ccda3e9cc1260b2e9385ee64004446c5ab83d
Sam Gleske <sam.mxracer@gmail.com>
2015-11-19 22:31:40 -0800

    add basic tests

---
+++ tests/api_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#Created by Sam Gleske
+#Meant to read from stdin a JSON blob from Jenkins master root
+import json
+import sys
+response = json.load(sys.stdin)
+jobs = map(lambda x: x["name"], response["jobs"])
+assert not response["useSecurity"]
+assert 'jervis' in jobs
+assert 'slack-plugin' in jobs
+assert 'Test' in jobs
